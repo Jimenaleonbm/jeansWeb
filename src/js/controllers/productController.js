e760c1818111c071138f582b432203b7901c417b
jimena martin leon <jimenaleonbm@gmail.com>
2016-07-20 13:47:39 -0500

    ajuste productos

--- src/js/controllers/productController.js
+++ src/js/controllers/productController.js
@@ -1,6 +1,51 @@
-var app = angular.module('app', [])
+angular.module('app')
+ 	.controller('productController', function ($scope) {
 
-	app.controller('productController', function(){
-		alert('app');
+ 		var ctrl = this;
 
+ 		ctrl.datos = [
+
+			{
+				nombre: "producto 1",
+				precio: "30.000",
+				imagen: "src/images/producto1.jpg",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod",
+				tallas: "S , M, L"
+			},
+			{
+				nombre: "producto 2",
+				precio: "30.000",
+				imagen: "src/images/producto2.jpg",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod",
+				tallas: "S , M, L"
+			},
+			{
+				nombre: "producto 3",
+				precio: "30.000",
+				imagen: "src/images/producto3.jpg",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod",
+				tallas: "S , M, L"
+			},
+			{
+				nombre: "producto 4",
+				precio: "30.000",
+				imagen: "src/images/producto4.jpg",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod",
+				tallas: "S , M, L"
+			},
+			{
+				nombre: "producto 5",
+				precio: "30.000",
+				imagen: "src/images/producto5.jpg",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod",
+				tallas: "S , M, L"
+			},
+			{
+				nombre: "producto 6",
+				precio: "30.000",
+				imagen: "src/images/producto6.jpg",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod",
+				tallas: "S , M, L"
+			}
+		]
 });
