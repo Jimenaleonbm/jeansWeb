4bc67cfab6810ef227820140751be3f43e870d11
jeison lopez <jeisonlopez@MacBook-Pro-de-jeison.local>
2016-10-01 19:24:02 -0500

    email cambios

--- public/app/checkout/checkout.js
+++ public/app/checkout/checkout.js
@@ -1,15 +1,24 @@
 (function (app) {
 
-    app.component("checkoutComponent",{
-        templateUrl:"app/checkout/checkout.html",
-        controllerAs:"vm",
-        controller:["cartService","$scope","$http",function (cartService,$scope,$http) {
-            
+    app.component("checkoutComponent", {
+        templateUrl: "app/checkout/checkout.html",
+        controllerAs: "vm",
+        controller: ["cartService", "$scope", "$http", function (cartService, $scope, $http) {
+
             var vm = this;
             vm.products = cartService.getProductsPurchase();
 
-            vm.sendEmail = function () {
-         
+            vm.sendEmail = function (user) {
+                user.message_html = "";
+                for(var i in vm.products){
+                    user.message_html += "Nombre " +vm.products[i].name + " Precio "+vm.products[i].price + " <br>";
+                }
+                emailjs.send("default_service", "template_43W8FQXP",user)
+                    .then(function (response) {
+                        console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+                    }, function (err) {
+                        console.log("FAILED. error=", err);
+                    });
             }
         }]
     })
